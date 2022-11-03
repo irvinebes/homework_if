9cfbc127add15b98b427f8653071cf6b909e6594
irvinebes <anya.shmakova.96@bk.ru>
2022-11-03 15:39:10 +0300

    Домашка 1 часть 2 с "else"

--- src/Main.java
+++ src/Main.java
@@ -5,39 +5,48 @@ public class Main {
         int age = 18;
         if (age >= 18) {
             System.out.println("Поздравляем с совершеннолетием");
-        }
-        if (age < 18) {
+        } else {
             System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать.");
         }
+
         // задание 2
         System.out.println("Задание 2");
-        int childAge = 8;
-        if (childAge >= 7) {
-            System.out.println("Ребёнок ходит в школу.");
+        int manAge = 6;
+        if (manAge >= 7) {
+            System.out.println("Ходит в школу, либо уже окончил.");
+        } else if (manAge < 7 ) {
+            System.out.println("Ребёнок ещё не ходит в школу.");
         }
-        int teenAge = 19;
-        if (teenAge >= 18) {
-            System.out.println("Человек уже закончил школу и может отправляться в университет.");
+        if (manAge >= 18) {
+            System.out.println("Человек может отправляться в университет.");
+        } else if (manAge < 18 ) {
+                System.out.println("Человеку ещё рано в университет.");
         }
-        int adultAge = 24;
-        if (adultAge >= 24) {
-            System.out.println("Человек окончил университет и ему пора искать первую работу.");
+        if (manAge >= 24) {
+            System.out.println("Человек окончил университет и ему пора искать работу.");
+        } else if (manAge < 24 ) {
+                System.out.println("Человеку ещё рано искать работу.");
         }
         // задание 3
         System.out.println("Задание 3");
-        int oneCar = 102;
+        int oneCar = 101;
         int seat = 59;
         int stand = 40;
         if (seat < 60) {
             System.out.println("Сидячие места ещё есть.");
+        } else {
+            System.out.println("Сидячих мест нет.");
         }
         if (stand < 42) {
             System.out.println("Стоячие места ещё есть.");
+        } else {
+            System.out.println("Стоячих мест нет.");
         }
         if (oneCar >= 102) {
             System.out.println("В вагоне нет места.");
+        } else {
+            System.out.println("В вагоне есть места.");
         }
-
 
 
     }
